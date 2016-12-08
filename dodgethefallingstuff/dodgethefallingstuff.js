fee7205268f21cfe79b7714652bf4f494ab08e08
Kaison Probst <kaisonpropst@gmail.com>
2016-12-08 16:54:21 -0500

    save

--- dodgethefallingstuff/dodgethefallingstuff.js
+++ dodgethefallingstuff/dodgethefallingstuff.js
@@ -1,4 +1,18 @@
-var game = new Phaser.Game (320,568);
+var C = {
+  "game": {
+    "width": 320,
+    "height": 568
+  },
+  "bg": {
+    "width": 320,
+    "height": 568,
+    "xspeed": 0,
+    "yspeed": 700,
+    "file": "assets/background.png"
+  }
+}
+
+ 
 class Boot {
   preload() {
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -13,14 +27,28 @@ class Boot {
 class Load {
   preload() {
     console.log("Loading...Please Wait");
+    this.load.image("bg",C.bg.file)
     //this.load.image("bg","assets/background.png")
   }
   
   create() {
     console.log("Thank you your game has Loaded");
+    this.state.start("Play")
   }
 }
 
+class Play {
+  create() {
+    console.log("Entered Play State");
+    this.background = this.add.tileSprite(0,0,320,568,"bg");
+    this.background.autoScroll(0,700);
+  }
+}
+
+
+ function restart() {
+var game = new Phaser.Game(320,568);
 game.state.add("Boot",Boot);
 game.state.add("Load",Load);
+game.state.add("Play",Play);
 game.state.start("Boot"); 
