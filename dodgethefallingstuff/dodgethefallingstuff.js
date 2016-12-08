7ac46b87d88d66c033523bad6f120fe9bb102158
Kaison Probst <kaisonpropst@gmail.com>
2016-12-08 16:22:22 -0500

    save

--- dodgethefallingstuff/dodgethefallingstuff.js
+++ dodgethefallingstuff/dodgethefallingstuff.js
@@ -1,1 +1,26 @@
-fdafad
+var game = new Phaser.Game (320,568);
+class Boot {
+  preload() {
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlginVertically = true;
+  }
+  create() {
+    this.state.start("Load")
+  }
+}
+
+class Load {
+  preload() {
+    console.log("Loading...Please Wait");
+    //this.load.image("bg","assets/background.png")
+  }
+  
+  create() {
+    console.log("Thank you your game has Loaded");
+  }
+}
+
+game.state.add("Boot",Boot);
+game.state.add("Load",Load);
+game.state.start("Boot"); 
